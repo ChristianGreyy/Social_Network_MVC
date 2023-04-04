510109969e1433463da0fbbb88b0fe1af935b509
ChristianGreyy <truongthanhhung2k2@gmail.com>
2023-04-04 19:37:21 +0700

    🛠  Update models patterns

--- src/models/index.js
+++ src/models/index.js
@@ -4,4 +4,4 @@ module.exports.Post = require("./post.model");
 module.exports.Comment = require("./comment.model");
 module.exports.Message = require("./message.model");
 module.exports.Document = require("./document.model");
-module.exports.Notification = require("./notification.model");
+module.exports.Notification = require("./notification.model.js");
