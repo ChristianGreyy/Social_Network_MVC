53972423453c08b9aaff7a2ddb23ac53d10434cc
ChristianGreyy <truongthanhhung2k2@gmail.com>
2023-04-04 11:57:00 +0700

    :wrench: Config file upload system

--- src/config/multer.js
+++ src/config/multer.js
@@ -52,17 +52,8 @@ const storage = multer.diskStorage({
 });
 
 function fileFilter(req, file, cb) {
-  // The function should call `cb` with a boolean
-  // to indicate if the file should be accepted
-
-  // To reject this file pass `false`, like so:
   cb(null, false);
-
-  // To accept the file pass `true`, like so:
   cb(null, true);
-
-  // You can always pass an error if something goes wrong:
-  // cb(new Error("I don't have a clue!"));
 }
 
 const upload = multer({ storage: storage });
